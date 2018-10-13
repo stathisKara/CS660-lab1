66e3b8b51feedf8041c94ba7ff535a0ea81620da
stathisKara <sdi1200057@di.uoa.gr>
2018-10-13 16:04:04 -0400

    updated seqscan with solutions code

--- src/java/simpledb/SeqScan.java
+++ src/java/simpledb/SeqScan.java
@@ -1,6 +1,5 @@
 package simpledb;
 
-import javax.xml.crypto.Data;
 import java.util.*;
 
 /**
@@ -12,68 +11,81 @@ public class SeqScan implements DbIterator {
 
     private static final long serialVersionUID = 1L;
 
-    private int tableid;
-    private HeapFile hf;
+    private boolean isOpen = false;
     private TransactionId tid;
-    private String tableAlias;
-    private String tableName;
-    private DbFileIterator db_it;
+    private TupleDesc myTd;
+    private transient DbFileIterator it;
+    //    private transient int tableid;
+    private String tablename;
+    private String alias;
 
     /**
      * Creates a sequential scan over the specified table as a part of the
      * specified transaction.
      *
-     * @param tid        The transaction this scan is running as a part of.
-     * @param tableid    the table to scan.
-     * @param tableAlias the alias of this table (needed by the parser); the returned
-     *                   tupleDesc should have fields with name tableAlias.fieldName
-     *                   (note: this class is not responsible for handling a case where
-     *                   tableAlias or fieldName are null. It shouldn't crash if they
-     *                   are, but the resulting name can be null.fieldName,
-     *                   tableAlias.null, or null.null).
+     * @param tid
+     *            The transaction this scan is running as a part of.
+     * @param tableid
+     *            the table to scan.
+     * @param tableAlias
+     *            the alias of this table (needed by the parser); the returned
+     *            tupleDesc should have fields with name tableAlias.fieldName
+     *            (note: this class is not responsible for handling a case where
+     *            tableAlias or fieldName are null. It shouldn't crash if they
+     *            are, but the resulting name can be null.fieldName,
+     *            tableAlias.null, or null.null).
      */
     public SeqScan(TransactionId tid, int tableid, String tableAlias) {
-        // some code goes here
         this.tid = tid;
-        this.tableid = tableid;
-        this.tableAlias = tableAlias;
-        this.hf = (HeapFile) Database.getCatalog().getDatabaseFile(tableid);
-        this.tableName = Database.getCatalog().getTableName(tableid);
-        this.db_it = hf.iterator(null);
+        reset(tableid,tableAlias);
     }
 
     /**
-     * @return return the table name of the table the operator scans. This should
-     * be the actual name of the table in the catalog of the database
-     */
+     * @return
+     *       return the table name of the table the operator scans. This should
+     *       be the actual name of the table in the catalog of the database
+     * */
     public String getTableName() {
-        return this.tableName;
+        return this.tablename;
     }
 
     /**
      * @return Return the alias of the table this operator scans.
-     */
-    public String getAlias() {
-        // some code goes here
-        return this.tableAlias;
+     * */
+    public String getAlias()
+    {
+        return this.alias;
     }
 
     /**
      * Reset the tableid, and tableAlias of this operator.
-     *
-     * @param tableid    the table to scan.
-     * @param tableAlias the alias of this table (needed by the parser); the returned
-     *                   tupleDesc should have fields with name tableAlias.fieldName
-     *                   (note: this class is not responsible for handling a case where
-     *                   tableAlias or fieldName are null. It shouldn't crash if they
-     *                   are, but the resulting name can be null.fieldName,
-     *                   tableAlias.null, or null.null).
+     * @param tableid
+     *            the table to scan.
+     * @param tableAlias
+     *            the alias of this table (needed by the parser); the returned
+     *            tupleDesc should have fields with name tableAlias.fieldName
+     *            (note: this class is not responsible for handling a case where
+     *            tableAlias or fieldName are null. It shouldn't crash if they
+     *            are, but the resulting name can be null.fieldName,
+     *            tableAlias.null, or null.null).
      */
     public void reset(int tableid, String tableAlias) {
-        // some code goes here
-        this.tableid = tableid;
-        this.tableAlias = tableAlias;
-        this.db_it = hf.iterator(null);
+//        this.tableid = tableid;
+        this.isOpen=false;
+        this.alias = tableAlias;
+        this.tablename = Database.getCatalog().getTableName(tableid);
+        this.it = Database.getCatalog().getDatabaseFile(tableid).iterator(tid);
+        myTd = Database.getCatalog().getTupleDesc(tableid);
+        String[] newNames = new String[myTd.numFields()];
+        Type[] newTypes = new Type[myTd.numFields()];
+        for (int i = 0; i < myTd.numFields(); i++) {
+            String name = myTd.getFieldName(i);
+            Type t = myTd.getFieldType(i);
+
+            newNames[i] = tableAlias + "." + name;
+            newTypes[i] = t;
+        }
+        myTd = new TupleDesc(newTypes, newNames);
     }
 
     public SeqScan(TransactionId tid, int tableid) {
@@ -81,8 +93,11 @@ public class SeqScan implements DbIterator {
     }
 
     public void open() throws DbException, TransactionAbortedException {
-        // some code goes here
-        this.db_it.open();
+        if (isOpen)
+            throw new DbException("double open on one DbIterator.");
+
+        it.open();
+        isOpen = true;
     }
 
     /**
@@ -93,42 +108,35 @@ public class SeqScan implements DbIterator {
      * (e.g., "alias.fieldName").
      *
      * @return the TupleDesc with field names from the underlying HeapFile,
-     * prefixed with the tableAlias string from the constructor.
+     *         prefixed with the tableAlias string from the constructor.
      */
     public TupleDesc getTupleDesc() {
-        TupleDesc td = Database.getCatalog().getTupleDesc(this.tableid);
-        Type[] types = new Type[td.getItems().length];
-        String[] fields = new String[td.getItems().length];
-        for (int i =0; i <fields.length; i++){
-            fields[i] = this.tableAlias.concat(td.getFieldName(i));
-            types[i] = td.getFieldType(i);
-        }
-        return new TupleDesc(types, fields);
+        return myTd;
     }
 
     public boolean hasNext() throws TransactionAbortedException, DbException {
-        // some code goes here
-        if (this.db_it == null)
-            return false;
-        if(!this.db_it.hasNext())
-            return false;
-        return true;
+        if (!isOpen)
+            throw new IllegalStateException("iterator is closed");
+        return it.hasNext();
     }
 
     public Tuple next() throws NoSuchElementException,
             TransactionAbortedException, DbException {
-        // some code goes here
-        return this.db_it.next();
+        if (!isOpen)
+            throw new IllegalStateException("iterator is closed");
+
+        return it.next();
+
     }
 
     public void close() {
-        // some code goes here
-        this.db_it.close();
+        it.close();
+        isOpen = false;
     }
 
     public void rewind() throws DbException, NoSuchElementException,
             TransactionAbortedException {
-        // some code goes here
-        this.db_it.rewind();
+        close();
+        open();
     }
 }
